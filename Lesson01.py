8ce69a7fb5048ce2f6c564c463a8777fdda81328
HoangBui <hoangbui1699@gmail.com>
2019-08-05 20:58:08 +0700

    The second commit to dev branch

--- Lesson01.py
+++ Lesson01.py
@@ -1,1 +1,14 @@
-print(2*[1])
+class Animal:
+    def __init__(self):
+        self.name = None
+        self.age = None
+
+    def show_info(self):
+        print('%s: %d' % (self.name, self.age))
+
+
+animal1 = Animal()
+animal1.name = 'Hoang'
+animal1.age = 20
+animal1.show_info()
+
